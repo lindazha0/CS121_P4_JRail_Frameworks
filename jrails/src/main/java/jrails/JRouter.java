0bac34c9297e729db5559462ba041acda6935e6f
Linda <zhaochy1@shanghaitech.edu.cn>
2022-11-17 02:28:48 -0500

    slightly modifies router

--- jrails/src/main/java/jrails/JRouter.java
+++ jrails/src/main/java/jrails/JRouter.java
@@ -1,22 +1,79 @@
 package jrails;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 
 public class JRouter {
 
+    /**
+     * mapping router path to controller method name
+     */
+    static Map<String, Map<String, String>> routes = new HashMap<>();
+    Class clazz;
+
+    /**
+     * @param verb: "GET/POST"
+     * @param path: "/path-to-page"
+     * @param clazz: which controller class to use
+     * @param method: controller methods
+     */
     public void addRoute(String verb, String path, Class clazz, String method) {
+        this.clazz = clazz;
         // Implement me!
+        switch(verb){
+            case "GET":
+                if(!routes.containsKey("GET")){
+                    Map<String, String> getRoutes = new HashMap<>();
+                    getRoutes.put(path, method);
+                    routes.put("GET", getRoutes);
+                }
+                routes.get("GET").put(path, method);
+                break;
+            case "POST":
+                break;
+            default:
+                // error!
+        }
     }
 
-    // Returns "clazz#method" corresponding to verb+URN
-    // Null if no such route
+    /**
+     * @param verb
+     * @param path
+     * @return "clazz#method" corresponding to verb+URN,
+     * Null if no such route
+     */
     public String getRoute(String verb, String path) {
-        throw new UnsupportedOperationException();
+        if(!routes.containsKey(verb)){
+            throw new UnsupportedOperationException();
+        }
+        if(!routes.get(verb).containsKey(path)){
+            throw new UnsupportedOperationException();
+        }
+
+        return clazz.getName() + "#" + routes.get(verb).get(path);
+
     }
 
-    // Call the appropriate controller method and
-    // return the result
+    /**
+     * Call the appropriate controller method and
+     * @param verb
+     * @param path
+     * @param params
+     * @return return the result
+     */
     public Html route(String verb, String path, Map<String, String> params) {
-        throw new UnsupportedOperationException();
+        Method m = clazz.getMethod(getRoute(verb, path));
+        Html result=null;
+
+        try {
+            result =  (Html) m.invoke(clazz, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+        // throw new UnsupportedOperationException();
     }
 }
