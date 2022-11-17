06309016898568701f914f2017cbd8d44c3ae050
Linda <zhaochy1@shanghaitech.edu.cn>
2022-11-17 14:55:21 -0500

    finish JRouter part

--- jrails/src/main/java/jrails/JRouter.java
+++ jrails/src/main/java/jrails/JRouter.java
@@ -1,9 +1,7 @@
 package jrails;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.*;
+import java.util.*;
 
 public class JRouter {
 
@@ -11,7 +9,7 @@ public class JRouter {
      * mapping router path to controller method name
      */
     static Map<String, Map<String, String>> routes = new HashMap<>();
-    Class clazz;
+    static String className;
 
     /**
      * @param verb: "GET/POST"
@@ -20,7 +18,7 @@ public class JRouter {
      * @param method: controller methods
      */
     public void addRoute(String verb, String path, Class clazz, String method) {
-        this.clazz = clazz;
+        className = clazz.getName();
         // Implement me!
         switch(verb){
             case "GET":
@@ -39,12 +37,12 @@ public class JRouter {
     }
 
     /**
+     * Helper function
      * @param verb
      * @param path
-     * @return "clazz#method" corresponding to verb+URN,
-     * Null if no such route
+     * @return controller#method name based on verb & path
      */
-    public String getRoute(String verb, String path) {
+    private String getControllerMethod(String verb, String path) {
         if(!routes.containsKey(verb)){
             throw new UnsupportedOperationException();
         }
@@ -52,8 +50,22 @@ public class JRouter {
             throw new UnsupportedOperationException();
         }
 
-        return clazz.getName() + "#" + routes.get(verb).get(path);
+        return routes.get(verb).get(path);
+    }
 
+
+    /**
+     * @param verb
+     * @param path
+     * @return "clazz#method" corresponding to verb+URN,
+     * Null if no such route
+     */
+    public String getRoute(String verb, String path) {
+        try{
+            return className + "#" + getControllerMethod(verb, path);
+        }catch(Exception e){
+            return null;
+        }
     }
 
     /**
@@ -64,16 +76,15 @@ public class JRouter {
      * @return return the result
      */
     public Html route(String verb, String path, Map<String, String> params) {
-        Method m = clazz.getMethod(getRoute(verb, path));
-        Html result=null;
-
         try {
-            result =  (Html) m.invoke(clazz, params);
+            Class<?> clazz = Class.forName(className);
+            Method m = clazz.getMethod(getControllerMethod(verb, path));
+            Html result =  (Html) m.invoke(clazz, params);
+            return result;
         } catch (Exception e) {
             e.printStackTrace();
+            throw e;
         }
-
-        return result;
         // throw new UnsupportedOperationException();
     }
 }
